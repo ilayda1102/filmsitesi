ac8fceb8dcce5c9c4c6fe63b79d07aeaf8420e2f
ilayda1102 <yusuf.say@agteks.com>
2026-07-29 17:27:30 +0300

    React login sistemi backend ile entegre edildi.

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { PrismaClient } = require("@prisma/client");
 const userRoutes = require("./routes/userRoutes");
+const cors = require("cors");
 
 const prisma = new PrismaClient();
 const app = express();
 
-
+app.use(cors());
 app.use(express.json());
 app.use("/", userRoutes);
 
